2709aa33b386ea0e4b283e0c68f3dd1a9e22c0ea
Ninesss <623840239@qq.com>
2025-09-22 21:23:35 +0800

    create 楽園

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import DecryptedText from './ReactBitsComponent/DecryptedText.jsx'
+
+function App() {
+
+  return (
+    <>
+      <DecryptedText
+        text="Hello! This is 楽園"
+        animateOn="view"
+        revealDirection="center"
+      />
+    </>
+  )
+}
+
+export default App
